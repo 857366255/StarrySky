d8d1ad2349ae7b830930348ef4aa330cca110eb3
857366255 <857366255@qq.com>
2017-10-07 10:27:40 +0800

    Initial commit

---
+++ src/main/java/com/starrysky/sys/vo/Basis.java
@@ -0,0 +1,16 @@
+package com.starrysky.sys.vo;
+
+import java.util.Date;
+
+/**
+ * 基础
+ * Created by wz on 2017/10/7.
+ */
+public class Basis {
+    private Integer id;
+    private Man createMan;
+    private Integer createManId;
+    private Date createTime;
+
+
+}
---
+++ src/main/java/com/starrysky/sys/vo/Man.java
@@ -0,0 +1,13 @@
+package com.starrysky.sys.vo;
+
+/**
+ * 人
+ * Created by wz on 2017/10/7.
+ */
+public class Man extends Basis{
+    private String name;
+    private Boolean sex;
+    private Integer age;
+    private String phone;
+    private String address;
+}
